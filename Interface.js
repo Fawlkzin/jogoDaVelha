f432bd27f73b71004e80f9457338aa5c1fa29384
Jefferson Silva <jeffersonrio77@gmail.com>
2022-01-03 18:17:35 -0300

    init

---
+++ Interface.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    let squares = document.querySelectorAll(".square")
+
+    squares.forEach((square)=>{
+        square.addEventListener("click", handleClick);
+    })
+
+})
+
+function handleClick(event){
+    console.log(event.target)
+
+    let square = event.target
+    let position = square.id
+
+    if(handleMove(position)){
+        setTimeout(()=>{
+            alert("O jogo acabou e o vencedir foi o " + playerTime)
+        },10)
+        
+    }
+    updateSquare(position)
+}
+
+function updateSquare(position){
+    let square = document.getElementById(position.toString());
+    let symbol = board[position];
+    square.innerHTML = `<div class = '${symbol}'></div>`
+    }
+
+function gameReset(){
+    location.reload()
+    return false
+}
